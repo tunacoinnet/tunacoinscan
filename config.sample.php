f5cbcf4f4e84365cbe0952083dfb8df2f731e461
TunaCoin Team <59313707+tunacoinnet@users.noreply.github.com>
2024-10-06 00:29:33 -0300

    Update config.sample.php

--- config.sample.php
+++ config.sample.php
@@ -36,8 +36,8 @@ define( 'W8IO_DB_DIR', __DIR__ . '/var/db/' );
 define( 'W8IO_DB_PATH', W8IO_DB_DIR . 'blockchain.sqlite3' );
 define( 'W8DB', 'sqlite:' . W8IO_DB_PATH );
 
-define( 'W8IO_NODES', 'http://127.0.0.1:6869|https://nodes.wavesexplorer.com' );
-define( 'W8IO_MATCHER', 'https://matcher.waves.exchange' );
+define( 'W8IO_NODES', 'http://seed2.tunacoin.org:5859|http://seed3.tunacoin.org:5859' );
+define( 'W8IO_MATCHER', 'http://nodes.tunacoin.org/matcher' );
 define( 'W8IO_NETWORK', 'W' ); // 'W' -- mainnet, 'T' -- testnet
 define( 'W8IO_ROOT', '/' );
 define( 'W8IO_MAX_UPDATE_BATCH', 1 ); // set more on when on a local node
